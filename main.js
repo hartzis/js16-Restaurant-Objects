1bff34051c695e7e7f74c486b1e65840648ac934
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-22 12:11:45 -0600

    added prototypes and super classes

--- main.js
+++ main.js
@@ -9,102 +9,103 @@ var Fooditem = function(name, calories, vegan, glutenFree, citrusFree) {
     this.calories = calories;
     this.vegan = vegan;
     this.glutenFree = glutenFree;
-    this.citrusFree = citrusFree;
-    this.toString = function() {
-        return "Name: " + this.name + " Calories: " + this.calories + " Is Vegan: " + this.vegan + " Is Gluten Free: " + this.glutenFree + " Is Citrus Free: " + this.citrusFree;
-
-    }
-}
-
-
-var Drink = function(name, description, price, ingredients) {
-
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.ingredients = ingredients;
-    this.toString = function() {
-        return "Name: " + this.name + " Description: " + this.description + " Price: " + this.price + " Ingredients: " + this.ingredients.map(function(z) {
-            return z.name;
-        });
-
-    }
+    this.citrusFree = citrusFree
+};
+Fooditem.prototype.toString = function() {
+    return "Name: " + this.name + " Calories: " + this.calories + " Is Vegan: " + this.vegan + " Is Gluten Free: " + this.glutenFree + " Is Citrus Free: " + this.citrusFree;
 };
 
-var Plate = function(name, description, price, ingredients) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.ingredients = ingredients;
-    this.toString = function() {
+var EdibleCombinations = (function() {
+
+    function EdibleCombinations(name, description, price, ingredients) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.ingredients = ingredients;
+    };
+    EdibleCombinations.prototype.toString = function() {
         return "Name: " + this.name + " Description: " + this.description + " Price: " + this.price + " Ingredients: " + this.ingredients.map(function(z) {
             return z.name;
         });
-    };
-    this.isVegan = function() {
-        return this.ingredients.map(function(z) {
+    }
+    EdibleCombinations.prototype.isVegan = function() {
+        return this.ingredients.every(function(z) {
             return z.vegan;
         })
     }
-    this.isGlutenFree = function() {
-        return this.ingredients.map(function(z) {
+    EdibleCombinations.prototype.isGlutenFree = function() {
+        return this.ingredients.every(function(z) {
             return z.glutenFree;
         })
     }
-    this.isCitrusFree = function() {
-        return this.ingredients.map(function(z) {
+    EdibleCombinations.prototype.isCitrusFree = function() {
+        return this.ingredients.every(function(z) {
             return z.citrusFree;
         })
     }
+    return EdibleCombinations;
+})();
 
+
+var Drink = function(name, description, price, ingredients) {
+    EdibleCombinations.call(this, name, description, price, ingredients);
+}
+Drink.prototype = new EdibleCombinations();
+
+var Plate = function(name, description, price, ingredients) {
+    EdibleCombinations.call(this, name, description, price, ingredients);
 };
+Plate.prototype = new EdibleCombinations();
+
+
+var ListsOfPlates = function(plates) {
+    this.plates = plates;
+}
+ListsOfPlates.prototype.toString = function() {
+    return this.plates.map(function(z) {
+        return "" + z.toString();
+    });
+}
 
 var Order = function(plates) {
-    this.plates = plates;
-    this.toString = function() {
-        return "Plates: " + this.plates.map(function(z) {
-            return z.name;
-        });
-
-    }
+    ListsOfPlates.call(this, plates);
 };
+Order.prototype = new ListsOfPlates();
+
 var Menu = function(plates) {
-    this.plates = plates;
-    this.toString = function() {
-        return "Plates:" + this.plates.map(function(z) {
-            return " " + z.name;
-        });
-
-    }
-
+    ListsOfPlates.call(this, plates);
 };
+Menu.prototype = new ListsOfPlates();
+
 var Restaurant = function(name, description, menu) {
     this.name = name;
     this.description = description;
     this.menu = menu;
-    this.toString = function() {
-        return "Plates:" + this.menu.plates.map(function(z) {
-            return " " + z.name;
-        });
 
-    }
 };
+Restaurant.prototype.toString = function() {
+    return "Menu has these Plates:" + this.menu.plates.map(function(z) {
+        return " " + z.name;
+    });
+
+}
 
 var Customer = function(diet) {
     this.dietaryPreference = diet;
-    this.toString = function() {
-        return "Dietary Preferences: " + this.dietaryPreference;
-    }
+
 };
+Customer.prototype.toString = function() {
+    return "Dietary Preferences: " + this.dietaryPreference;
+}
 
 //instantiate all variables
 
 //ingredients
 var cornChips = new Fooditem('Corn Chips', 200, false, true, false);
-var groundBeef = new Fooditem('Beef Patty', 500, false, true, true);
+var groundBeef = new Fooditem('Ground Beef', 500, false, true, true);
 var tomato = new Fooditem("tomato", 25, true, true, true);
 var avacados = new Fooditem("avacados", 55, true, true, true);
-var wheatTortilla = new Fooditem("tortilla", 425, false, false, false);
+var wheatTortilla = new Fooditem("tortilla", 425, false, false, true);
 var tequilla = new Fooditem("tequilla", 25, true, true, true);
 var limeJuice = new Fooditem("tequilla", 25, true, true, false);
 
